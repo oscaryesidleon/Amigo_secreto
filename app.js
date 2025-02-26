2a0a91af7f7b646e0c1b0b9887778ac11e8621fe
Oscar Yesid León <oscar.leon03@est.uexternado.edu.co>
2025-02-25 22:38:48 -0500

    Update challenge app.js

--- app.js
+++ app.js
@@ -1,1 +1,20 @@
-console.log("Ejecutando el sistema")
+let listaAmigos = [];
+let campoAmigo = document.getElementById("amigo");
+let ulListaAmigos = document.getElementById("listaAmigos");
+let ulResultado = document.getElementById("resultado");
+
+function agregarAmigo() {
+    if (campoAmigo.value === "") {
+        alert("Por favor, inserte un nombre");
+    } else {
+        let nombreAmigo = campoAmigo.value;
+        listaAmigos.push(nombreAmigo);
+        ulListaAmigos.innerHTML += `<li>${nombreAmigo}</li>`;
+        campoAmigo.value = "";
+    }
+}
+
+function sortearAmigo() {
+    let amigoSecreto = listaAmigos[Math.floor(Math.random() * listaAmigos.length)];
+    ulResultado.innerHTML = `<li>El amigo secreto es: ${amigoSecreto}</li>`;
+}
